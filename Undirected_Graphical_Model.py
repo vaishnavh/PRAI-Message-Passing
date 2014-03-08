613c316d0d0224e7917a9cdbf94f3d38f0de8a2a
vaishnavh <vaishnavh.nagarajan@gmail.com>
2014-03-09 04:32:33 +0530

    Added variable elimination code

--- Undirected_Graphical_Model.py
+++ Undirected_Graphical_Model.py
@@ -21,16 +21,53 @@ class Undirected_Graphical_Model():
 
     def is_chain(self):
         """
-        Checks whether the given graph is a chain and returns true (or false) respectively.
+        Checks whether the given graph is a chain and returns true (or False) respectively.
         """
+        node_names = [node.name  for node in self.nodes]
+        factor_names = [factor.name for factor in self.factors]
+        node_count = dict()  # A count of how many times a
+        #node has occured in the factor
+        for factor_name in factor_names:
+            if len(factor_name) > 2:
+                # A chain cannot have a factor of more than three
+                # variables
+                return False
+            elif len(factor_name) == 2:
+                # Don't look at single variable factors!
+                for i in xrange(2):
+                    if factor_name[i] in node_count.keys():
+                        if node_count[factor_name[i]] == 2:
+                            # Break if a variable has occured
+                            # twice already!
+                            return False
+                        node_count[factor_name[i]] += 1
+                    else:
+                        node_count[factor_name[i]] = 1
+        leaves = 0
+        for node_name in node_names:
+            if node_name not in node_count.keys():
+                # The node doesn't occur in any factor.
+                # Must not occur ideally.
+                return False
+            elif node_count[node_name] > 2:
+                # Node occurs in more than two two-sized
+                # factors.
+                return False
+            elif node_count[node_name] == 1:
+                # Node occurs in exactly one two=sized
+                # factor. Could be a leaf node.
+                leaves += 1
+        return (leaves == 2)  # Exactly two leaves must exist!
+
+
 
 
     def normalize(self, factor):
         """
         Normalizes the values corresponding to the given factor and returns the normalized factor.
         """
-	sum_of_factors = sum(factor.values)
-	return [x/sum_of_factors for x in factor.values]
+        sum_of_factors = sum(factor.values)
+        return Factor.Factor(factor.name, [float(x)/float(sum_of_factors) for x in factor.values])
 
 
 
@@ -38,6 +75,30 @@ class Undirected_Graphical_Model():
         """
         Multiplies the given two factors and returns the resultant factor.
         """
+        nodes_1 = [c for c in factor_1.name]  # Nodes participating in factor 1
+        nodes_2 = [c for c in factor_2.name]  # Nodes participating in factor 2
+        nodes = list(set(nodes_1 + nodes_2))  # Gets only the unique nodes participating
+        # in both factors
+        product_name = ''.join(nodes)
+        product_values = [0] * pow(2, len(product_name))
+
+        for pos in xrange(len(product_values)):  # For each row in the factor table
+            pos_1 = 0  # What row in factor_1 table?
+            pos_2 = 0  # What row in factor_2 table?
+            for node_pos in xrange(len(nodes)):  # For each node in the product factor
+                if pos & pow(2, len(nodes) - 1 - node_pos) != 0:
+                    # The current node in the product, is set as 'f'
+                    if nodes[node_pos] in nodes_1:
+                        node_1_pos = nodes_1.index(nodes[node_pos])
+                        pos_1 += pow(2, len(nodes_1) - 1 - node_1_pos)
+                        # Narrow down to the correct row
+                    if nodes[node_pos] in nodes_2:
+                        node_2_pos = nodes_2.index(nodes[node_pos])
+                        pos_2 += pow(2, len(nodes_2) - 1 - node_2_pos)
+                        # Narrow down to the correct row
+            product_values[pos] = factor_1.values[pos_1] * factor_2.values[pos_2]
+        return Factor.Factor(product_name, product_values)
+
 
 
 
@@ -46,12 +107,30 @@ class Undirected_Graphical_Model():
         Marginalizes the given factor over the given variable and returns the marginalized factor.
         """
 
+        var_pos =  factor.name.index(variable) # TODO: Node or variable!?
+        marg_nodes = [c for c in factor.name]
+        marg_nodes = marg_nodes[:var_pos] + marg_nodes[(var_pos+1):] # Remove variable from the list
+        marg_values = [0] * pow(2, len(marg_nodes))
+        for pos in xrange(len(factor.values)):
+            marg_pos = 0  # Where this corresponds to in the marginal table
+            for node_pos in xrange(len(factor.name)):
+                if (node_pos != var_pos) and (pos & pow(2, len(factor.name) - 1 - node_pos) != 0):
+                    marg_node_pos =  marg_nodes.index(factor.name[node_pos])
+                    marg_pos += pow(2, len(marg_nodes) - 1 - marg_node_pos)
+            marg_values[marg_pos] += factor.values[pos]  #Sums over variable
+        return Factor.Factor(''.join(marg_nodes), marg_values)
+
+
+
+
 
     def get_elimination_ordering(self, query_variables):
         """
         Returns an elimination ordering of the random variables after removing the query variables as a list.
         For example, if random variables are 'B', 'C', 'A' and query variable is 'B', then one elimination order is ['C', 'A'].
         """
+        node_names = [node.name for node in self.nodes]
+        return list(set(node_names) - set(query_variables))
 
 
     def marginal_inference(self, variable):
@@ -59,6 +138,31 @@ class Undirected_Graphical_Model():
         Performs marginal inference, P(variable), on the UGM and returns the marginal probability distribution as a list.
         """
         elimination_order = self.get_elimination_ordering(variable)
+        temp_factors = self.factors
+        for var_pos in xrange(len(elimination_order)):
+            # Eliminate this variable
+            factors_involved = [factor for factor in temp_factors if (elimination_order[var_pos] in factor.name)]
+            # The above line finds factors that involve the to-be-eliminated
+            # variable
+            temp_factors = list(set(temp_factors) - set(factors_involved))
+            # temp_factors is updated with the remaining factors
+            if len(factors_involved) > 0:
+                # There is atleast one factor involved
+                # Multiply all these factors
+                product_factor = factors_involved[len(factors_involved)-1] # Get the last factor
+                for factor_pos in xrange(len(factors_involved)-1):
+                    product_factor = self.multiply_factors(product_factor, factors_involved[factor_pos])
+                # Marginalize over this factor on the  to-be-eliminated variable
+                product_factor = self.marginalize(product_factor, elimination_order[var_pos])
+                temp_factors = temp_factors + [product_factor] # Add back the product factor
+        # Finally on eliminating all the other variables, we might be left with a
+        # set of factors dependent only on 'variable'. We need to multiply
+        # them.
+        marginal = temp_factors[len(temp_factors)-1]
+        for factor_pos in xrange(len(temp_factors)-1):
+            marginal = self.multiply_factors(marginal, temp_factors[factor_pos])
+        return marginal
+
 
 
     def joint_inference(self, variable_1, variable_2):
@@ -66,6 +170,32 @@ class Undirected_Graphical_Model():
         Performs joint inference, P(variable_1, variable_2), on the UGM and returns the joint probability distribution as a list.
         """
         elimination_order = self.get_elimination_ordering([variable_1, variable_2])
+        temp_factors = self.factors
+        for var_pos in xrange(len(elimination_order)):
+            # Eliminate this variable
+            factors_involved = [factor for factor in temp_factors if (elimination_order[var_pos] in factor.name)]
+            # The above line finds factors that involve the to-be-eliminated
+            # variable
+            temp_factors = list(set(temp_factors) - set(factors_involved))
+            # temp_factors is updated with the remaining factors
+            if len(factors_involved) > 0:
+                # There is atleast one factor involved
+                # Multiply all these factors
+                product_factor = factors_involved[len(factors_involved)-1] # Get the last factor
+                for factor_pos in xrange(len(factors_involved)-1):
+                    product_factor = self.multiply_factors(product_factor, factors_involved[factor_pos])
+                # Marginalize over this factor on the  to-be-eliminated variable
+                product_factor = self.marginalize(product_factor, elimination_order[var_pos])
+                temp_factors = temp_factors + [product_factor] # Add back the product factor
+        # Finally on eliminating all the other variables, we might be left with a
+        # set of factors dependent only on 'variable_1' and 'variable_2'. We need to multiply
+        # them.
+        marginal = temp_factors[len(temp_factors)-1]
+        for factor_pos in xrange(len(temp_factors)-1):
+            marginal = self.multiply_factors(marginal, temp_factors[factor_pos])
+        return marginal
+
+
 
 
     def conditional_inference(self, variable_1, variable_2):
--- Utils.py
+++ Utils.py
@@ -11,6 +11,66 @@ def moralize_graph(input_file):
     """
     Performs moralization of the given graph if directed and returns the list of Factor objects (factors).
     """
+    f = open(input_file)  # Opens the input file
+    lines = f.readlines()  # Reads lines of the file and stores it in a list
+    lines = filter(None, [line.strip(' \n\t') for line in lines])  # Strips the lines
+    # of whitespaces and compresses the list
+    lines = lines[4:]  # Removes first four dummy lines
+
+    # Replacing \t in the string
+    for i in xrange(len(lines)):
+        new_string = str()
+        for c in lines[i]:
+            if c == '\t':
+                new_string += ' '
+            else:
+                new_string += c
+        # new_string has only space separated terms
+        lines[i] = new_string
+    # All string in lines have only space separated terms
+
+    factors = dict()
+    i = 0
+    while i < len(lines):
+        # Go through the table of factors
+        # The first line must be of the form P(A,B,C...) which we shall
+        # overlook
+        child_node = lines[i].split()[0][2:]
+        child_true = lines[i].split()[2][0]
+        i += 1  # Skips the header line (? TODO)
+        # How many variables are there!? Read the next line! :)
+        if len(lines[i].split()) == 1:
+            # We've read something like P(B = t)
+            pos = child_true == 'f'
+            factor_values = [0] * 2
+            factor_values[pos] = float(lines[i])
+            factor_values[1 - pos] = 1 - float(lines[i])
+            factors[child_node] = Factor.Factor(child_node, factor_values)
+            i += 1
+        else:
+            n_vars = len(lines[i + 1].split()) - 1
+            vars_in_factor = lines[i].split()[0:n_vars]
+            factor_name = ''.join(vars_in_factor)  # ['A','B','C'] -> 'ABC'
+            factor_name = child_node + factor_name
+            j = i + 1
+            i += pow(2, n_vars) + 1  # Number of entries expected in table + header
+            factor_values = [0] * pow(2, n_vars + 1)
+            while j < i:
+                row = lines[j].split()
+                row_pos = row[:n_vars]  # A list of t t f etc,
+                row_val = float(row[n_vars])  # Value of the factor
+                pos = (child_true == 'f')
+                n_pos = 1 - pos
+                for k in xrange(len(row_pos)):
+                    pos = (pos * 2) + (row_pos[k] == 'f')
+                    n_pos = (n_pos * 2) + (row_pos[k] == 'f')
+                factor_values[pos] = row_val
+                factor_values[n_pos] = 1 - row_val
+                j += 1
+            factors[factor_name] = Factor.Factor(factor_name, factor_values)
+    return factors.values()
+
+
 
 
 def read_file(input_file):
@@ -21,11 +81,97 @@ def read_file(input_file):
     Creates a list of objects (nodes) of class Node corresponding to the random variables in the graph.
     Returns the list of Factor() objects (factors) and the list of Node() objects.
     """
-    f = open(input_file) #Opens the input file
-    lines = f.readlines() #Reads lines of the file and stores it in a list
-    lines = filter(None,[line.strip(' \n\t') for line in lines]) #Strips the lines of whitespaces and compresse
-    #s the list
-    if lines[1] == 'DIRECTED':
+    f = open(input_file)  # Opens the input file
+    lines = f.readlines()  # Reads lines of the file and stores it in a list
+    lines = filter(None, [line.strip(' \n\t') for line in lines])  # Strips the lines
+    # of whitespaces and compresses the list
+    is_directed = 0
+    if lines[0] == 'DIRECTED':
+        is_directed = 1
+    lines = lines[4:]  # Removes first four dummy lines
+    nodes = dict()
+    # Replacing \t in the string
+    for i in xrange(len(lines)):
+        new_string = str()
+        for c in lines[i]:
+            if c == '\t':
+                new_string += ' '
+            else:
+                new_string += c
+        # new_string has only space separated terms
+        lines[i] = new_string
+    # All string in lines have only space separated terms
+
+    #Reading nodes
+    i = 0
+    while i < len(lines):
+        # Go through the table of factors
+        # The first line must be of the form P(A,B,C...) which we shall
+        # overlook
+        # If the graph is directed, we've to consider the node J in
+        # P(J = t| ... )
+        if is_directed == 1:
+            child_node = lines[i].split()[0][2:]
+            if not nodes.has_key(child_node):
+                nodes[child_node] = Node.Node(child_node, Message.Message([1, 1]), Message.Message([1, 1]))  # TODO
+        i += 1  # Skips the header line (? TODO)
+        if len(lines[i].split()) > 1:  # To skip cases like P(B = t)
+            # How many variables are there!? Read the next line! :)
+            n_vars = len(lines[i + 1].split()) - 1
+            vars_in_factor = lines[i].split()[0:n_vars]
+            for var_in_factor in vars_in_factor:
+                # If the factor has not been recorded yet...
+                if not nodes.has_key(var_in_factor):
+                    nodes[var_in_factor] = Node.Node(var_in_factor, Message.Message([1, 1]), Message.Message([1, 1]))  # TODO
+            j = i + 1
+            i += pow(2, n_vars)  # Number of entries expected in table + header
+            #nodes now contains all the nodes as a dictionary
+        i += 1 # Regardless of the case being P(B = t) or P(B = t | A, C) or
+        # P(A, B, C) ...
+    if is_directed == 1:
+        return [moralize_graph(input_file), nodes.values()]
+    else:
+        #Undirected
+        factors = dict()
+        i = 0
+        while i < len(lines):
+            # Go through the table of factors
+            # The first line must be of the form P(A,B,C...) which we shall
+            # overlook
+            i += 1  # Skips the header line (? TODO)
+            # How many variables are there!? Read the next line! :)
+            n_vars = len(lines[i + 1].split()) - 1
+            vars_in_factor = lines[i].split()[0:n_vars]
+            factor_name = ''.join(vars_in_factor)  # ['A','B','C'] -> 'ABC'
+            j = i + 1
+            i += pow(2, n_vars) + 1  # Number of entries expected in table + header
+            factor_values = [0] * pow(2, n_vars)
+            while j < i:
+                row = lines[j].split()
+                row_pos = row[:n_vars]  # A list of t t f etc,
+                row_val = row[n_vars]  # Value of the factor
+                pos = 0
+                for k in xrange(len(row_pos)):
+                    pos = (pos * 2) + (row_pos[k] == 'f')
+                factor_values[pos] = row_val
+                j += 1
+            factors[factor_name] = Factor.Factor(factor_name, factor_values)
+        return [factors.values(), nodes.values()]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
